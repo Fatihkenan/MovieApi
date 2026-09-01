46eff23fd0f4d7f3dd4cc7df378162968d38d287
Fatih Kenan <fkaya253412@gmail.com>
2026-09-01 09:58:35 +0300

    varlıkların tamamlanması ve contextin oluşturulması

---
+++ Core/MovieApi.Domain/Entities/Cast.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieApi.Domain.Entities
+{
+    public class Cast
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Overview { get; set; }
+        public string? Biography { get; set; }
+
+    }
+}
---
+++ Infrastructure/Persistence/Context/MovieContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+
+namespace Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;initial Catalog = ApiMovieDb; Integrated Security=True;TrustServerCertificate=True");
+        }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+
+
+    }
+}

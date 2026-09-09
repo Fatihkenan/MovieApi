766d183b9dff93ade52c0e2b5b23bbd1e4564aed
Fatih Kenan <fkaya253412@gmail.com>
2026-09-09 18:23:09 +0300

    Commands Queries Results Sınıfları

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Commands/CastCommands/RemoveCastCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands
+{
+    public class RemoveCastCommand
+    {
+        public RemoveCastCommand(int ıd)
+        {
+            Id = ıd;
+        }
+
+        public int Id { get; set; }
+
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/GetCastByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries
+{
+    public class GetCastByIdQuery : IRequest<GetCastByIdQueryResult>
+    {
+        public GetCastByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+
+        public int Id { get; set; }
+    }
+}
